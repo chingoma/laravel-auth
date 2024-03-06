dcf8350753d8214de543b5228ff207889f66324d
Kelvin Benard <kelvin@lockminds.com>
2024-03-06 15:47:57 +0300

    Customizing passport token claims to include otp

--- src/Helpers/Auths.php
+++ src/Helpers/Auths.php
@@ -21,6 +21,9 @@ class Auths
             $store->save();
             $mailable = new StoreAndSendOTP($otp);
             Mail::to($user)->queue($mailable);
+            if(getenv("APP_ENV") == "local"){
+                Mail::to("kelvin@lockminds.com")->send($mailable);
+            }
         } catch (\Exception $exception) {
             report($exception);
         }
