3aacb67cab329af657ca65eddc5bededee624fc4
chingoma <chingoma@users.noreply.github.com>
2024-03-13 17:06:01 +0000

    Fix styling

--- src/Controllers/AccessController.php
+++ src/Controllers/AccessController.php
@@ -37,7 +37,7 @@ class AccessController extends BaseAccessController
 
             //get user
             $user = DB::table('users')
-                ->select(['id', 'email','firstname'])
+                ->select(['id', 'email', 'firstname'])
                 ->where('email', '=', $username)
                 ->first();
 
@@ -70,7 +70,7 @@ class AccessController extends BaseAccessController
                 'token' => $data['access_token'],
                 'name' => $user->firstname,
                 'email' => $user->email,
-                'role' => "",
+                'role' => '',
                 'permissions' => [],
                 'access_token' => $data['access_token'],
                 'refresh_token' => $data['refresh_token'],
