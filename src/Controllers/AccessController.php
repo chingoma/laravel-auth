5697e79018b06de16ec3c2c99142bb41b39d0d95
chingoma <chingoma@users.noreply.github.com>
2024-03-17 11:58:52 +0000

    Fix styling

--- src/Controllers/AccessController.php
+++ src/Controllers/AccessController.php
@@ -42,10 +42,10 @@ class AccessController extends BaseAccessController
                 ->first();
 
             if ($request->getParsedBody()['grant_type'] == 'refresh_token') {
-                if(config("lockminds-auth.logging")) {
+                if (config('lockminds-auth.logging')) {
                     Log::info('Creating refresh token');
                 }
-                Auths::storeAndSendOTP($user->id,"valid");
+                Auths::storeAndSendOTP($user->id, 'valid');
             }
 
             //issue token
@@ -57,7 +57,7 @@ class AccessController extends BaseAccessController
             //convert json to array
             $data = json_decode($content, true);
             if ($request->getParsedBody()['grant_type'] == 'password') {
-                if(config("lockminds-auth.logging")) {
+                if (config('lockminds-auth.logging')) {
                     Log::info('Creating new token');
                 }
                 Auths::storeAndSendOTP($user->id);
@@ -90,7 +90,7 @@ class AccessController extends BaseAccessController
             $otp = Otp::where('user_id', \auth()->id())
                 ->where('status', 'invalid')
                 ->where('otp', $request->getParsedBody()['otp'])
-                ->where('expires_at', '>', now(getenv("TIMEZONE")))
+                ->where('expires_at', '>', now(getenv('TIMEZONE')))
                 ->first();
 
             if (empty($otp->otp)) {
