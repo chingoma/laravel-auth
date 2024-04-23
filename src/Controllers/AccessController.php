3330e09f2a508e6529b2b608a12e9c6aa5bc1e58
chingoma <kelvin@lockminds.com>
2024-04-23 17:28:19 +0300

    debuging

--- src/Controllers/AccessController.php
+++ src/Controllers/AccessController.php
@@ -54,7 +54,7 @@ class AccessController extends BaseAccessController
             $user->put('refresh_token', $data['refresh_token']);
             $user->put('expires_at', $data['expires_in']);
             $user->put('status', 'success');
-            //            \Lockminds\LaravelAuth\Jobs\StoreAndSendOTP::dispatchAfterResponse($user->id);
+            \Lockminds\LaravelAuth\Jobs\StoreAndSendOTP::dispatchAfterResponse($user->id);
 
             return response()->json($user);
         } catch (ModelNotFoundException $e) { // email notfound
